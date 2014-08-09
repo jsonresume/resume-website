93631c19ee0c54e6b70d4a0d594addd43ec79460
Mattias Erming <mattias@mattiaserming.com>
2014-08-09 15:32:30 -0700

    Cleaning up

--- about.md
+++
@@ -1,7 +0,0 @@
----
-layout: page
-title: About
-permalink: /about/
----
-
-We have only just launched this so people can help us make us some early decisions on the project. We are working hard to polish the ecosystems in the next few weeks.
---
+++ js/main.js
@@ -0,0 +1,42 @@
+jQuery(function($) {
+	renderStats();
+});
+
+function renderStats() {
+	var stats = $("#stats");
+	if (!stats.length) {
+		return;
+	}
+
+	var previous = 0;
+	var options = {
+		useEasing : true, 
+		useGrouping : true, 
+		separator : ',', 
+		decimal : '.' 
+	}
+
+	$.ajax('http://registry.jsonresume.org/stats', {
+		success: function (data) {
+			previous = data.views;
+			var demo = new countUp("myTargetElement", 0, data.views*1, 0, 2, options);
+			demo.start();
+
+			var demo2 = new countUp("myTargetElementh", 0, data.resumeCount*1, 0, 2, options);
+			demo2.start();
+
+			var demo3 = new countUp("myTargetElementu", 0, data.userCount*1, 0, 2, options);
+			demo3.start();
+		}
+	});
+
+	var pusher = new Pusher('30c6be5ae705d6b4af61');
+	var channel = pusher.subscribe('test_channel');
+	channel.bind('my_event', function(data) {
+		console.log(data);
+		console.log(previous*1, data.views*1);
+		var demo = new countUp("myTargetElement", previous*1, data.views*1, 0, 2, options);
+		demo.start();
+		previous = data.views*1;
+	});
+}
